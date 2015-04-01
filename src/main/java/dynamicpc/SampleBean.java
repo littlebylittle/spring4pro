e67e4ed50c25a5601fcd78fc0b86389d4be74f95
I.Mashnikov <imashnikov+wms-WMS-config@solvo.ru>
2015-04-01 17:34:41 +0300

    dynamic pointcut example

---
+++ src/main/java/dynamicpc/SampleBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dynamicpc;
+
+/**  @author igorm  */
+public class SampleBean {
+	public void foo(int i) {
+		System.out.println("You call `foo` method with: `" + i + "` argument");
+	}
+	public void bar() {
+		System.out.println("You call `bar` method");
+	}
+}
