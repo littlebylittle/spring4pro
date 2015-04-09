77b2cc754e7e12dd45f987c371fa8739817918ae
I.Mashnikov <imashnikov+wms-WMS-config@solvo.ru>
2015-04-09 16:20:25 +0300

    Some little tests with aop-proxy factory bean, creating proxy-factory, tests setter-injection

--- src/main/java/pfb/TestMainClass.java
+++ src/main/java/pfb/TestMainClass.java
@@ -1,6 +1,8 @@
 package pfb;
 
+import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,7 +14,7 @@ import org.springframework.aop.framework.ProxyFactoryBean;
 public class TestMainClass {
 	public static void main(String[] args) {
 		ProxyFactoryBean pfb = new ProxyFactoryBean();
-		pfb.setTarget(args);
+		pfb.setInterceptorNames(new String[]{"bean"});
+		DefaultPointcutAdvisor pc = new DefaultPointcutAdvisor();
 	}
-
 }
